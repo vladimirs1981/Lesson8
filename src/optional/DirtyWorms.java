1488410e0612115b13787252e0777e70d9a058b5
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-06-23 21:38:27 +0200

    DirtyWorms

--- src/optional/DirtyWorms.java
+++ src/optional/DirtyWorms.java
@@ -1,13 +1,18 @@
 package optional;
+
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class DirtyWorms {
-	
+
 	public static void main(String[] args) {
 
-		class Stuff {}
-		class Worm extends Stuff {}
-		class Dirt extends Stuff {}
+		class Stuff {
+		}
+		class Worm extends Stuff {
+		}
+		class Dirt extends Stuff {
+		}
 		ArrayList<Stuff> stuffIFoundInTheYard = new ArrayList<Stuff>();
 		stuffIFoundInTheYard.add(new Worm());
 		stuffIFoundInTheYard.add(new Dirt());
@@ -18,10 +23,12 @@ public class DirtyWorms {
 
 		System.out.println(stuffIFoundInTheYard.size());
 
-		/* TODO: Clean out the dirt but keep the delicious worms. */ 
+		stuffIFoundInTheYard.remove(1);
+		stuffIFoundInTheYard.remove(1);
+		stuffIFoundInTheYard.remove(1);
+		stuffIFoundInTheYard.remove(2);
 
-		System.out.println(stuffIFoundInTheYard.size());	//should be 2
+		System.out.println(stuffIFoundInTheYard.size()); // should be 2
 
 	}
 }
-
