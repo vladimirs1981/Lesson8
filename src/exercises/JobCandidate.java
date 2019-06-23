7f0ba113f731757b723f43bde1ca207f6d3340be
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-06-23 21:32:18 +0200

    JobCandidate

--- src/exercises/JobCandidate.java
+++ src/exercises/JobCandidate.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 
 import java.util.List;
 
-public class JobCandidate {
+public class JobCandidate implements Comparable<JobCandidate>{
 
 	private List<String> personalityTraits;
 	private int hoursSpentCoding;
@@ -24,9 +24,29 @@ public class JobCandidate {
 		this.teacherName = teacherName;
 		this.salaryRequired = salaryRequired;
 	}
-	
+
 	public static void main(String[] args) {
-		JobCandidate june = new JobCandidate(Arrays.asList(new String[] {"adaptable", "scatty", "curious"}), 650, "Vinny Wade", 1200000); 
+
+		JobCandidate june = new JobCandidate(Arrays.asList(new String[] { "adaptable", "scatty", "curious" }), 1200,
+				"Vinny Wade", 1200000);
+		JobCandidate vlada = new JobCandidate(Arrays.asList(new String[] { "open", "fair", "humorous" }), 700,
+				"Joon Spoon", 1200);
+
+		if (june.compareTo(vlada)==1) {
+			System.out.println("June is better job candidate");
+		}else if(june.compareTo(vlada)==-1){
+			System.out.println("Vlada is better job candidate.");
+		}else {
+			System.out.println("They are equal. Use random to choose your candidate");
+		}
+
+
+
 	}
 
+	@Override
+	public int compareTo(JobCandidate other) {
+		
+		return Integer.compare(this.hoursSpentCoding, other.hoursSpentCoding);
+	}
 }
